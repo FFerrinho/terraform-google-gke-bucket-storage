ccf279f516c7c60f79d08ef402a957b23495b140
Francisco Ferrinho <ferrinho@gmail.com>
2025-03-03 10:00:04 +0000

    Enhance GCS Fuse CSI module with optional Kubernetes Service Account creation and improved variable descriptions

--- main.tf
+++ main.tf
@@ -1,43 +1,59 @@
+# Get the project information needed for various resource configurations
 data "google_project" "main" {
   project_id = var.project_id
 }
 
-# Create the Kubernetes Service Account for GCS Fuse CSI
+# Create a Kubernetes Service Account for GCS Fuse CSI if requested
+# This is optional as the module can work with pre-existing service accounts
+# The service account is annotated to work with GCP Workload Identity
 resource "kubernetes_service_account_v1" "main" {
+  count    = var.create_k8s_service_account ? 1 : 0
   provider = kubernetes
   metadata {
-    name      = var.k8s_service_account
+    name      = var.k8s_service_account_name
     namespace = var.k8s_namespace
     annotations = {
-      "iam.gke.io/gcp-service-account" = var.gke_service_account
+      "iam.gke.io/gcp-service-account" = var.gke_service_account_email
     }
   }
 }
 
-# Bind the Kubernetes Service Account to the Google Cloud Service Account
+# Configure Workload Identity by binding the Kubernetes ServiceAccount to the GCP ServiceAccount
+# This binding allows the K8s SA to impersonate the GCP SA using Workload Identity
 resource "google_service_account_iam_binding" "gcs-fuse-csi-sa" {
-  service_account_id = "${data.google_project.main.id}/serviceAccounts/${var.gke_service_account}"
+  count              = var.create_k8s_service_account ? 1 : 0
+  service_account_id = "${data.google_project.main.id}/serviceAccounts/${var.gke_service_account_email}"
   role               = "roles/iam.workloadIdentityUser"
-  members            = ["serviceAccount:${data.google_project.main.project_id}.svc.id.goog[${kubernetes_service_account_v1.main.id}]"]
+  members            = ["serviceAccount:${data.google_project.main.project_id}.svc.id.goog[${var.k8s_namespace}/${var.k8s_service_account_name}]"]
 }
 
-# Define the members for the storage bucket
+# Configure the members that need access to the GCS bucket
+# When creating a new K8s SA, both the GCP SA and Workload Identity pool need access
+# When using existing SA, only the GCP SA needs access
 locals {
-  storage_bucket_members = [
-    "serviceAccount:${var.gke_service_account}",
-    "principal://iam.googleapis.com/projects/${data.google_project.main.number}/locations/global/workloadIdentityPools/${data.google_project.main.project_id}.svc.id.goog/subject/ns/bsp/sa/${kubernetes_service_account_v1.main.metadata[0].name}"
+  storage_bucket_members = var.create_k8s_service_account ? [
+    "serviceAccount:${var.gke_service_account_email}",
+    "principal://iam.googleapis.com/projects/${data.google_project.main.number}/locations/global/workloadIdentityPools/${data.google_project.main.project_id}.svc.id.goog/subject/ns/${var.k8s_namespace}/sa/${var.k8s_service_account_name}"
+  ] : [
+    "serviceAccount:${var.gke_service_account_email}"
   ]
 }
 
-# Grant access to the storage bucket
+# Grant storage.objectAdmin permissions to the configured members
+# This allows both read and write access to objects in the bucket
 resource "google_storage_bucket_iam_member" "main" {
   for_each = toset(local.storage_bucket_members)
   bucket   = var.bucket_name
   role     = "roles/storage.objectAdmin"
   member   = each.key
+
+  depends_on = [
+    kubernetes_service_account_v1.main
+  ]
 }
 
-# Create the Storage Class for GCS Fuse CSI
+# Create a Kubernetes StorageClass for GCS Fuse CSI driver
+# This defines how PersistentVolumes will be dynamically provisioned
 resource "kubernetes_storage_class_v1" "main" {
   provider = kubernetes
   metadata {
@@ -47,7 +63,8 @@ resource "kubernetes_storage_class_v1" "main" {
   allow_volume_expansion = var.k8s_storage_class_allow_storage_expansion
 }
 
-# Create the Persistent Volume for GCS Fuse CSI
+# Create a PersistentVolume that uses the GCS Fuse CSI driver
+# This PV will be statically provisioned and bound to a specific GCS bucket
 resource "kubernetes_persistent_volume" "main" {
   provider = kubernetes
   metadata {
@@ -77,12 +94,13 @@ resource "kubernetes_persistent_volume" "main" {
     mount_options = var.k8s_pv_mount_options
   }
 
+  # Ensure IAM permissions are set before creating the PV
   depends_on = [google_storage_bucket_iam_member.main]
 }
 
-# Create the Persistent Volume Claim for GCS Fuse CSI
+# Create a PersistentVolumeClaim that will be bound to the PV
+# This PVC can be referenced by pods to mount the GCS bucket
 resource "kubernetes_persistent_volume_claim_v1" "main" {
-
   metadata {
     name      = var.k8s_pv_claim_name
     namespace = var.k8s_namespace
@@ -99,6 +117,7 @@ resource "kubernetes_persistent_volume_claim_v1" "main" {
     volume_name        = kubernetes_persistent_volume.main.id
   }
 
+  # Ensure PV exists before creating PVC
   depends_on = [
     kubernetes_persistent_volume.main
   ]
--- variables.tf
+++ variables.tf
@@ -1,75 +1,80 @@
 variable "project_id" {
-  description = "The project ID."
+  description = "The Google Cloud Project ID where resources will be created."
   type        = string
 }
 
-variable "gke_service_account" {
-  description = "The name of the GKE Service Account used by the nodes. Can be a custom or default service account."
+variable "gke_service_account_email" {
+  description = "The email of the GCP Service Account used by GKE nodes. This can be either the default compute service account or a custom service account."
   type        = string
 }
 
 variable "gke_cluster_name" {
-  description = "The name of the GKE Cluster."
+  description = "The name of the GKE Cluster where the GCS Fuse CSI driver is installed."
   type        = string
 }
 
 variable "bucket_name" {
-  description = "The name of the Google Cloud Storage Bucket."
+  description = "The name of the existing Google Cloud Storage Bucket to mount."
   type        = string
 }
 
-variable "k8s_service_account" {
-  description = "The name of the Kubernetes Service Account."
+variable "k8s_service_account_name" {
+  description = "The name of the Kubernetes Service Account that will be used to access the GCS bucket through Workload Identity."
   type        = string
-  default     = "gcs-storage-sa"
+}
+
+variable "create_k8s_service_account" {
+  description = "Whether to create a new Kubernetes Service Account and bind it to the GCP Service Account. Set to false if you already have a service account configured with Workload Identity."
+  type        = bool
+  default     = true
 }
 
 variable "k8s_namespace" {
-  description = "The name of the Kubernetes Namespace."
+  description = "The Kubernetes Namespace where the Service Account, PVC, and other resources will be created."
   type        = string
 }
 
 variable "k8s_storage_class" {
-  description = "The name of the Kubernetes Storage Class."
+  description = "The name of the Kubernetes Storage Class that will be created for the GCS Fuse CSI driver."
   type        = string
 }
 
 variable "k8s_storage_class_allow_storage_expansion" {
-  description = "Allow volume expansion."
+  description = "Whether to allow volume expansion in the Storage Class. This enables dynamic resizing of persistent volumes."
   type        = bool
   default     = true
 }
 
 variable "k8s_persistent_volume_name" {
-  description = "The name of the Kubernetes Persistent Volume."
+  description = "The name of the Kubernetes Persistent Volume that will be created to represent the GCS bucket."
   type        = string
 }
 
 variable "k8s_pv_access_modes" {
-  description = "The access modes for the Persistent Volume."
+  description = "The access modes for the Persistent Volume. Default is ReadWriteMany to allow multiple pods to mount the same bucket."
   type        = list(string)
   default     = ["ReadWriteMany"]
 }
 
 variable "k8s_pv_capacity" {
-  description = "The capacity for the Persistent Volume."
+  description = "The storage capacity to report for the Persistent Volume. This doesn't limit the actual GCS bucket size."
   type        = string
   default     = "100Gi"
 }
 
 variable "k8s_pv_read_only" {
-  description = "The read only status for the Persistent Volume."
+  description = "Whether to mount the GCS bucket as read-only in the Persistent Volume."
   type        = bool
   default     = false
 }
 
 variable "k8s_pv_claim_name" {
-  description = "The name of the Persistent Volume Claim."
+  description = "The name of the Persistent Volume Claim that will be created to bind to the Persistent Volume."
   type        = string
 }
 
 variable "k8s_pv_mount_options" {
-  description = "The mount options for the Persistent Volume."
+  description = "Mount options for the GCS Fuse CSI driver. Default includes implicit-dirs for better directory handling and uid/gid 33 for www-data user compatibility."
   type        = list(string)
-  default     = ["implicit-dirs", "uid=33", "gid=33"]
+  default     = ["implicit-dirs"]
 }
